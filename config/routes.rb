ced34d57f57e04d2a44e8c53a475ff3ff90a243e
Chad Guidry <chadjguidry@gmail.com>
2015-09-04 14:46:58 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
 
@@ -20,7 +20,7 @@ SampleApp::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  # Sample resource route (maps HTTP verbs to controller actions automatically)
   #   resources :products
 
   # Sample resource route with options:
